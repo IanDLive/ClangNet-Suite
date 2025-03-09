f252d1838fd252b0df74fdcad2028bbb0e39173e
James <Itsame@userguy1.com>
2025-03-09 02:05:14 +0000

    Add chuck jokes API
    Return of the chuck norris facts to discord. !chuckl

--- scripts/discord/custom/chuckjokes-discord.js
+++ scripts/discord/custom/chuckjokes-discord.js
@@ -11,32 +11,25 @@
         var arguments = event.getArguments();
         var args = event.getArgs();
 
-        // Decision loops set up to parse an argument that is passed with the command.
-        // The first conditional statement also checks as to whether the command originated
-        //     in the '#games-room' channel of the Discord server.  This can be changed to whatever
-        //     chat room on your server you want if you want to lock it down to one channel to reduce
-        //     spam in all or any other channels, or remove that part of the condition to make it 
-        //     open to all.
-        // The second conditional statement checks to see whenther the 'total' parameter was passed
-        //     to the command - this returns the total number of jokes in the database.
-        // The third conditional statement applies the number that has been entered with the command
-        //     to return the specific joke.  If a number is not supplied then a default error from the 
-        //     API is returned.
-        // If no argument is passed, then a random joke is returned.
-        if (command.equalsIgnoreCase('chuck') && channel.equalsIgnoreCase('games-room')) {
-            if (arguments.equalsIgnoreCase('total')) {
-                jsonObject = JSON.parse($.cnGetJSON('https://api.icndb.com/jokes/count'));
-                $.discord.say(channel, $.lang.get('chuckjokes.totaljokes', jsonObject.value));
-                return;
-            }
-            if (args[0] !== undefined) {
-                jsonObject = JSON.parse($.cnGetJSON('https://api.icndb.com/jokes/' + args[0]));
-                $.discord.say(channel, $.lang.get('chuckjokes.joke', jsonObject.value.id, jsonObject.value.joke));
-            } else {
-                jsonObject = JSON.parse($.cnGetJSON('https://api.icndb.com/jokes/random'));
-                $.discord.say(channel, $.lang.get('chuckjokes.joke', jsonObject.value.id, jsonObject.value.joke));
-            }
+        // Get a list of all of the Chuck Norris joke categories.
+        function getChuckCategories() {
+            var jsonObject;
+            jsonObject = JSON.parse($.cnGetJSON('https://api.chucknorris.io/jokes/categories'));
+            return jsonObject;
         }
+
+        // Get a single random Chuck Norris joke.
+        function getChuck() {
+            var jsonObject;
+            jsonObject = JSON.parse($.cnGetJSON('https://api.chucknorris.io/jokes/random'));
+            return jsonObject.value;
+        }
+
+        // Command event to check for the 'chuck' command.
+        if (command.equalsIgnoreCase('chuck') && channel.equalsIgnoreCase('games-room')) {
+            $.discord.say(channel, $.lang.get('clangnetjokes.telljoke.discord', getChuck()));
+        }
+
     });
 
     // initReady event to register the commands.
