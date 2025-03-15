342bbf0c3a3c084e14a4bdb5d05f3a9459d2101e
James <Itsame@userguy1.com>
2025-03-15 00:08:21 +0000

    Update wenstream-discord.js
    Add stream status

--- scripts/discord/custom/wenstream-discord.js
+++ scripts/discord/custom/wenstream-discord.js
@@ -18,6 +18,11 @@
             var snarkyComment = Math.floor(Math.random() * 6);
 
             var twitchCache = Packages.tv.phantombot.cache.TwitchCache;
+            if (twitchCache != null) {
+                $.discord.say(channel, twitchCache.getStreamStatus());
+            }
+
+       
 
             // If not online, send response
 
